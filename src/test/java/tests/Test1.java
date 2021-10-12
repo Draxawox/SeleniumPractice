f24dfd2e338d45354533212f74cea2b7641c1b47
Draxawox <kalix321@gmail.com>
2021-10-12 09:56:11 +0200

    first commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import BaseTest.BaseAbstractChromeTest;
+import BaseTest.BaseAbstractTest;
+import org.testng.annotations.Test;
+
+
+public class Test1 extends BaseAbstractChromeTest {
+
+    @Test
+    public void test() {
+        int a = 5+6;
+    }
+}
